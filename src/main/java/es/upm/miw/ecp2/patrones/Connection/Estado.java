f5e24f1b7ceff0a3278a8cce686f38fd03a8bb9b
bl0823 <bl0823@192.168.7.20>
2014-10-09 09:53:32 +0200

    Archivos paquete state.connection

---
+++ src/main/java/es/upm/miw/ecp2/patrones/Connection/Estado.java
@@ -0,0 +1,5 @@
+package es.upm.miw.ecp2.patrones.Connection;
+
+public enum Estado {
+    CERRADO, PARADO, PREPARADO, ESPERANDO;
+}
---
+++ src/main/java/es/upm/miw/ecp2/patrones/Connection/Link.java
@@ -0,0 +1,8 @@
+package es.upm.miw.ecp2.patrones.Connection;
+
+public interface Link {
+    static final int ACK = 0;
+
+    void enviar(String msg);
+    void recibir (int respuesta);
+}
