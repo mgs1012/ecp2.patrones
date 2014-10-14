7e74409eca8e0b190ead1bd41c0793d8bb25f961
bl0823 <bl0823@192.168.7.20>
2014-10-14 13:20:52 +0200

    Añadir clases implementar patrón Memento

---
+++ src/main/java/es/upm/miw/ecp2/patrones/calculadora/CalculadoraMementable.java
@@ -0,0 +1,14 @@
+package es.upm.miw.ecp2.patrones.calculadora;
+
+public class CalculadoraMementable extends Calculadora implements MementableI{
+	
+	public void guardar(){
+		
+	}
+	
+	public void deshacer(){
+		
+	}
+	
+
+}
